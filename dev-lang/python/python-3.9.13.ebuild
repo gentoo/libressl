gentoo/libressl

commit 12c52eb5ce4b9cae9ff0383efb18a54035002fa2

dev-lang/python: Merge changes from ::gentoo
Signed-off-by: orbea <orbea@riseup.net>
Closes: https://github.com/gentoo/libressl/pull/426
Signed-off-by: Quentin Retornaz <gentoo@retornaz.com>

dev-lang/python/python-3.9.13.ebuild
@@ -25,6 +25,7 @@ S="${WORKDIR}/${MY_P}"
 
 LICENSE="PSF-2"
 SLOT="${PYVER}"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc ~x86"
 KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 IUSE="bluetooth build examples gdbm hardened lto +ncurses pgo +readline +sqlite +ssl test tk wininst +xml"
 RESTRICT="!test? ( test )"

commit 0f30243161dffa4de4ae711e611639242431d88d

dev-lang/python: Fix keywords
Fixes: 12c52eb5ce4b9cae9ff0383efb18a54035002fa2
Signed-off-by: orbea <orbea@riseup.net>

dev-lang/python/python-3.9.13.ebuild
@@ -26,7 +26,6 @@ S="${WORKDIR}/${MY_P}"
 LICENSE="PSF-2"
 SLOT="${PYVER}"
 KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc ~x86"
-KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 IUSE="bluetooth build examples gdbm hardened lto +ncurses pgo +readline +sqlite +ssl test tk wininst +xml"
 RESTRICT="!test? ( test )"
 