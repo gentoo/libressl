1cea73eae5768915086a7891628438865d7664df
orbea <orbea@riseup.net>
2023-07-16 09:37:51 -0700

    dev-lang/rust: don't use grep -P
    Closes: https://github.com/gentoo/libressl/issues/534
    Signed-off-by: orbea <orbea@riseup.net>

--- dev-lang/rust/files/rehash-crate.sh
+++ dev-lang/rust/files/rehash-crate.sh
@@ -7,8 +7,8 @@ cargo='.cargo-checksum.json'
 
 grep -- '+++' "${1:?}" | while read -r _ f; do
 	file="${f#*/}"
-	orig_sum="$(grep -Po "(?<=\"${file}\":\")[0-9a-fA-F]+(?=\")" \
-		"${cargo}")"
+	orig_sum="$(grep -Eo "\"${file}\":\"[0-9a-fA-F]+\"" "${cargo}" |
+		cut -d':' -f2 | tr -d '"')"
 	if [ -n "${orig_sum}" ]; then
 		sum="$(sha256sum "${file}")"
 		sed -i "s|${orig_sum}|${sum%% *}|" "${cargo}"
